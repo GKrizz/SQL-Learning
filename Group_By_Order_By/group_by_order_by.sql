d53f46ed46d93551e0c8b4996fffde4737a57a44
gobalakrishnan21cb17 <kbaskarankbaskaran72@gmail.com>
2025-02-17 15:00:21 +0530

    Group by and order by

---
+++ Group_By_Order_By/group_by_order_by.sql
@@ -0,0 +1,3 @@
+-- select name, mark from student order by mark asc;
+-- select avg(mark) as mark, department from student group by department;
+-- select count(name) as count, department from student group by department order by count desc;
