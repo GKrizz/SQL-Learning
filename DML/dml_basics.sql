ccd1aa62fd619bd42f9228c9f75d7e9563a946a4
gobalakrishnan21cb17 <kbaskarankbaskaran72@gmail.com>
2025-02-17 14:59:35 +0530

    DML

---
+++ DML/dml_basics.sql
@@ -0,0 +1,16 @@
+-- CREATE TABLE student(name VARCHAR(100), department VARCHAR(100), roll_num VARCHAR(100));
+
+-- INSERT INTO student VALUES ('Gobal', 'CSBS', '21CB17');
+-- INSERT INTO student VALUES ('Sanjeevi', 'CSBS', '21CB46');
+-- INSERT INTO student VALUES ('Jeban', 'CSE', '21CB23');
+
+-- UPDATE student SET department='CSE' WHERE roll_num='21CB23';
+
+-- alter table student add mark int;
+
+-- update student set mark=90 where name='gobal';
+-- update student set mark=99 where name='Jeban';
+-- update student set mark=86 where name='Sanjeevi';
+
+-- INSERT INTO student VALUES ('Vijay', 'ECE', '21ECE62',80);
+-- INSERT INTO student VALUES ('Krish', 'AIDS', '21AI30',74);
